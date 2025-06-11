8e62072af1f72294633ddb18d063e27d60f00e84
KimHang1504 <160225729+KimHang1504@users.noreply.github.com>
2025-06-11 23:07:01 +0700

    clear route

--- backend/routes/consultationBooking.route.js
+++ backend/routes/consultationBooking.route.js
@@ -7,4 +7,7 @@ router.post('/', consultationBooking.createBooking);
 router.get('/:id', consultationBooking.getBookingById);
 router.put('/:id', consultationBooking.updateBooking);
 router.delete('/:id', consultationBooking.deleteBooking);
+
+router.get('/customer/:accountId', consultationBooking.getBookingsByCustomerAccount);
+router.get('/counselor/:accountId', consultationBooking.getBookingsByCounselorAccount);
 module.exports = router;
