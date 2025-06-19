2f4b9e93a09549505993c86cd15bb20e9767be6d
Khoa <khoa1509gg@gmail.com>
2025-06-19 13:55:31 +0700

    add more bad words

--- backend/utils/filterWords.js
+++ backend/utils/filterWords.js
@@ -1,3 +1,20 @@
-module.exports=[
-  "địt", "fuck", "chết", "lừa đảo", "thuốc giảm cân", "hot girl", "xxx", "dm", "tự tử"
+module.exports = [
+  // Từ thô tục, xúc phạm
+  "địt", "dm", "vkl", "cl", "cmm", "mẹ mày", "bố mày", "thằng ngu", "con điên", "đồ chó", "óc chó",
+  "cút", "câm mồm", "ngu vãi", "vô học", "đồ rác rưởi", "thằng lồn", "con đĩ", "thằng khốn", "chó chết",
+
+  // Từ tục tĩu, khiêu dâm
+  "fuck", "sex", "xxx", "bú", "liếm", "thẩm du", "đụ", "dâm", "thịt nhau", "chịch", "sờ mó", "làm tình",
+  "nứng", "kích dục", "jav", "phim sex", "porn", "gái gọi", "gái bán dâm", "trai bao", "dâm đãng",
+
+  // Bạo lực, kích động tự hại
+  "chết", "tự tử", "cắt tay", "tự sát", "giết người", "treo cổ", "uống thuốc độc", "hủy hoại bản thân",
+
+  // Lừa đảo, quảng cáo không phù hợp
+  "lừa đảo", "đa cấp", "bán thuốc", "thuốc giảm cân", "thuốc kích dục", "hack nick", "kiếm tiền nhanh",
+  "chơi forex", "đầu tư ảo", "đánh bạc", "lô đề", "casino", "nổ hũ", "game đỏ đen",
+
+  // Spam, câu view
+  "hot girl", "link 18+", "click ngay", "xem thêm", "sốc chưa", "giảm giá", "quà tặng", "mua ngay",
+  "kiếm tiền tại nhà", "thu nhập khủng", "việc nhẹ lương cao", "quảng cáo", "quảng bá", "spam", "câu like", "câu view", "câu follow", "câu sub", "câu tương tác"
 ];
