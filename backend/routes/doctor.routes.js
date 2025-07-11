2993bb0f57c216a0b8352a9c73b3c2131b0b4dd3
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-07-11 21:40:23 +0700

    Fix Authen Doctor, DoctorTestService, TestService

--- backend/routes/doctor.routes.js
+++ backend/routes/doctor.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ctrl = require("../controllers/doctor.controller");
 const router = express.Router();
 const { authenticate, authorize } = require("../middlewares/auth.middleware");
-router.use(authenticate, authorize("Admin"));
+router.use(authenticate, authorize("Customer"));
 
 router.post("/", ctrl.create);
 router.get("/", ctrl.getAll);
